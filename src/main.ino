b8627ab1ad0b3d043360095359d79c2fd7127e5e
fexd12 <felipe.demelo@hotmail.com>
2020-04-08 12:47:09 -0300

    adicionado menu

--- src/main.ino
+++ src/main.ino
@@ -1,18 +1,18 @@
+#include <HTTPClient.h>
 #include <Arduino.h>
 #include "parson.h"
-#include "AzureIotHub.h"
-#include "Esp32MQTTClient.h"
 #include <WiFi.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <string.h>
 
-#define RST_PIN 5 // pin rfid
-#define SS_PIN 15 // pin rfid
+#define RST_PIN 22 // pin rfid
+#define SS_PIN 21 // pin rfid
 
-const char *ssid = "Enter ssid";
-const char *password = "ssid password";
-const char *host = "Enter host Ex: google.com";
+String BASE_URL = "http://192.168.0.28:3000/";
+
+char *ssid = "Fe";
+const char *password = "q1w2e3r4t5";
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
 								  /*
@@ -26,12 +26,16 @@ String c;
 String d;
 
 WiFiClient client;
+HTTPClient http;
+
+void dump_byte_array(byte *buffer, byte bufferSize);
 
 void setup(){
 	Serial.begin(115200); // Initialize serial communications with the PC
 
 	SPI.begin();		// Init SPI bus
 	mfrc522.PCD_Init(); // Init MFRC522 card
+  
 	//for wifi
 	Serial.print("Connecting to ");
 	Serial.println(ssid);
@@ -47,20 +51,12 @@ void setup(){
 	Serial.println(WiFi.localIP());
 	Serial.println("Getway: ");
 	Serial.println(WiFi.gatewayIP());
-
+  
 	Serial.println(F("RFID Read: "));
 }
 
 void loop(){
-	//Connection to server
-	Serial.print("Connecting to: ");
-	Serial.println(host);
 
-	const int httpPort = 80;
-	if (!client.connect(host, httpPort)){
-		Serial.println("Connection failed");
-		return;
-	}
 
 	// Look for new cards
 	if (!mfrc522.PICC_IsNewCardPresent()){
@@ -72,8 +68,47 @@ void loop(){
 		delay(50);
 		return;
 	}
+
+	int opcao= menu();
+
+	if(opcao == 0){
+		leituraDados();
+	}
+	else if(opcao == 1){
+		GravaDados();
+	}
+	else{
+		Serial.println('Opcao incorreta');
+		return;
+	}
+	
+
 	// Show some details of the PICC (that is: the tag/card)
-	Serial.print(F("Card UID:"));
+	// Serial.print(F("Card UID:"));
+	// dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+	// Serial.println();
+
+	// data = mfrc522.uid.uidByte, mfrc522.uid.size;
+	// for (int i = 0; i < 4; i++){
+	// 	Serial.print(data[i], HEX);
+	// }
+	// a = String(data[0], HEX);
+	// b = String(data[1], HEX);
+	// c = String(data[2], HEX);
+	// d = String(data[3], HEX);
+
+	// a.toUpperCase();
+	// b.toUpperCase();
+	// c.toUpperCase();
+	// d.toUpperCase();
+	// String tag = String(a)+String(b)+String(c)+String(d);
+	// //Data sending
+	// httpRequest("tag",tag);
+	// httpGetAgendamento("agendamento?tag=" +tag);
+}
+
+void leituraDados(){
+	Serial.print("Card UID:");
 	dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
 	Serial.println();
 
@@ -90,30 +125,120 @@ void loop(){
 	b.toUpperCase();
 	c.toUpperCase();
 	d.toUpperCase();
+	
+	String tag = String(a)+String(b)+String(c)+String(d);
 
-	//Data sending
-	String url = "/tag/" + String(a) + String(b) + String(c) + String(d);
-	Serial.println("Requesting URL  ");
-	//Serial.println(url);
+	char response = httpGetAgendamento("agendamento?tag=" +tag);
+}
 
-	client.print(String("POST ") + url + " HTTP/1.0\r\n" +
-				 "Host: " + host + "\r\n" +
-				 "Connection: close\r\n\r\n");
-
-	delay(5000);
-	while (client.available()){
-		String line = client.readStringUntil('\r');
-		Serial.print(line);
-	}
+void GravaDados(){
+	Serial.print("Card UID:");
+	dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
 	Serial.println();
-	Serial.println("Closing connection");
-	delay(5000);
+
+	data = mfrc522.uid.uidByte, mfrc522.uid.size;
+	for (int i = 0; i < 4; i++){
+		Serial.print(data[i], HEX);
+	}
+	a = String(data[0], HEX);
+	b = String(data[1], HEX);
+	c = String(data[2], HEX);
+	d = String(data[3], HEX);
+
+	a.toUpperCase();
+	b.toUpperCase();
+	c.toUpperCase();
+	d.toUpperCase();
+	
+	String tag = String(a)+String(b)+String(c)+String(d);
+	httpRequest("tag",tag);
+}
+
+int menu(){
+	Serial.println(F("\nEscolha uma opcao:"));
+	Serial.println(F("0 - leitura da Tag"));
+	Serial.println(F("1 - Salva Tag no banco"));
+
+	while(!Serial.available()){}
+
+	int op = (int)Serial.read();
+	while(Serial.available()){
+		if (Serial.read()=='\n')break;
+		Serial.read();	
+	}
+	return (op-48);
+}
+
+void httpRequest(String path, String payload){
+  String dados = PostTag(path,payload);
+
+  if (!dados) {
+    return;
+  }
+
+  Serial.println("##[RESULT]## ==> " + dados);
+
+}
+
+String PostTag(String path,String payload){
+  http.begin(BASE_URL + path);
+  http.addHeader("content-type", "application/x-www-form-urlencoded");
+
+  String body = "tag="+payload;
+
+  int httpCode = http.POST(body);
+
+  if (httpCode < 0) {
+    Serial.println("request error - " + httpCode);
+    return "error";
+
+  }
+
+  if (httpCode != HTTP_CODE_OK) {
+    return "";
+  }
+
+  String response =  http.getString();
+  http.end();
+
+  return response;
+}
+
+void httpGetAgendamento(String path){
+  String dados = GetAgendamento(path);
+
+  if (!dados) {
+    return;
+  }
+
+  Serial.println("##[RESULT]## ==> " + dados);
+
+}
+
+String GetAgendamento(String path){
+	http.begin(BASE_URL + path);
+	int httpCode = http.GET();
+
+	if (httpCode < 0) {
+    Serial.println("request error - " + httpCode);
+    return "error";
+
+  }
+
+  if (httpCode != HTTP_CODE_OK) {
+    return "";
+  }
+
+  String response =  http.getString();
+  http.end();
+
+  return response;
 }
 
 // Helper routine to dump a byte array as hex values to Serial
 void dump_byte_array(byte *buffer, byte bufferSize){
 	for (byte i = 0; i < bufferSize; i++){
-		Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+		//Serial.print(buffer[i] < 0x10 ? " 0" : " ");
 		Serial.print(buffer[i], HEX);
 	}
 }
